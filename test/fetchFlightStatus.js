8c6621e2344f51e12800d7f5e6aa96ba7d39422d
Thomas Whitehill <tom@redskyresearch.com>
2022-02-22 09:42:47 -0500

    Messing with Server

---
+++ test/fetchFlightStatus.js
@@ -0,0 +1,38 @@
+var Test = require('../config/testConfig.js');
+const truffleAssert = require('truffle-assertions');
+
+
+//var BigNumber = require('bignumber.js');
+
+contract('Oracles', async (accounts) => {
+
+    const TEST_ORACLES_COUNT = 20;
+
+    //  var config;
+    before('setup contract', async () => {
+        config = await Test.Config(accounts);
+    });
+
+    it('Test: Request Flight Status', async () => {
+
+        // ARRANGE
+        let flight = 'ND1309'; // Course number
+        let timestamp = Math.floor(Date.now() / 1000);
+        let indexForThisFlightStatus;
+
+        // Submit a request for oracles to get status information for a flight
+        let result = await config.flightSuretyApp.fetchFlightStatus(config.firstAirline, flight, timestamp);
+        console.log(`Transaction ${result.tx}`);
+        // filter only events of OracleRegistered or whatever
+        truffleAssert.prettyPrintEmittedEvents(result, 2);
+
+        let events = result.logs.filter((entry) => entry.event === 'OracleRequest');
+
+        let eventObject = JSON.parse(JSON.stringify(events[0]));
+        truffleAssert.eventEmitted(result, 'OracleRequest', (ev) => {
+            indexForThisFlightStatus = ev.index;
+            console.log("Received expected OracleRequest Event with index " + indexForThisFlightStatus);
+            return (ev.flight === flight && ev.timestamp == timestamp);
+        });
+    });
+});
